e62922c0df352a4c7e2c35765c7b45490effe9d9
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-03-01 23:59:07 +0530

    Update Q2b.asm

--- Assginments/MP/Assg2/Q2b.asm
+++ Assginments/MP/Assg2/Q2b.asm
@@ -1,29 +1,45 @@
 data segment
-str1 db 'ABS'
-len1 equ ($-str1)
-str2 db 'ABC'
-len2 equ ($-str2)
-res db 0h
+str1 db 15,?,15 dup(0)
+str2 db 15,?,15 dup(0)
+gre db 'greater$'
+lower db 'lesser$'
 data ends
 code segment
 assume cs:code,ds:data,es:data
 start:mov ax,data
 mov ds,ax
 mov es,ax
-mov cx,len1
 cld
-lea si,str1
-lea di,str2
+
+mov dx,offset str1
+mov ah,0ah
+int 21h
+mov cx,str1+1
+lea si,str1+2
+
+mov ah,02h
+mov dl,0ah
+int 21h
+
+mov dx,offset str2
+mov ah,0ah
+int 21h
+lea di,str2+2
 
 l1:cmpsb
 jg greater
 jl less
 loop l1
 
-greater:mov res,01h
+greater:
+mov dx,offset gre
+mov ah,09h
+int 21h
 jmp last
 
-less:mov res,0fh
+less:mov dx,offset lower
+mov ah,09h
+int 21h
 jmp last
 
 last:mov ah,4ch
